0aa4c0a93ee93596e0f453788e57c443d47c23a2
mehsan <ma469234@gmail.com>
2024-07-18 10:45:33 +0500

    update

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -10,7 +10,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
     <style>
-        body, html {
+      body,
+        html {
             height: 100%;
             margin: 0;
             overflow: hidden;
@@ -20,6 +21,15 @@
             height: 100vh;
             overflow-y: scroll;
             scroll-snap-type: y mandatory;
+            padding: 0;
+        }
+
+        .video-item video {
+            max-height: 100%;
+            max-width: 100%;
+            height: inherit;
+            object-fit: cover;
+            cursor: pointer;
         }
 
         .video-item {
@@ -33,15 +43,10 @@
             margin: auto;
         }
 
-        .video-item video {
-            max-height: 100%;
-            max-width: 100%;
-            height: inherit;
-            object-fit: cover;
-            cursor: pointer;
-        }
 
-        .video-actions, .volume-control {
+
+        .video-actions,
+        .volume-control {
             position: absolute;
             right: 10px;
             bottom: 84px;
@@ -50,7 +55,8 @@
             align-items: center;
         }
 
-        .video-actions button, .volume-control input {
+        .video-actions button,
+        .volume-control input {
             margin-bottom: 10px;
             background-color: rgba(0, 0, 0, 0.5);
             border: none;
@@ -64,12 +70,24 @@
             justify-content: center;
         }
 
+        .volume-control {
+            right: 80px;
+            display: none;
+        }
+
         .volume-slider {
+            -webkit-appearance: none;
             width: 100px;
             height: 5px;
             background: rgba(255, 255, 255, 0.7);
-            cursor: pointer;
+            outline: none;
+            opacity: 0.7;
             transition: opacity .15s ease-in-out;
+            cursor: pointer;
+        }
+
+        .volume-slider:hover {
+            opacity: 1;
         }
 
         .play-pause-btn {
@@ -98,11 +116,43 @@
             z-index: 99;
         }
 
-        .share-popup, .overlay {
-            display: none;
+        .videos_actions_bx {
+            display: flex;
+            flex-direction: column;
         }
 
+        .share-btn {
+            cursor: pointer;
+        }
+
+        .modal-content {
+            border: none;
+        }
+
+        .share-popup {
+            display: none;
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            background: #fff;
+            border: 1px solid #ccc;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            z-index: 1000;
+            width: 12%;
+            max-width: 300px;
+        }
+
+        @media (max-width: 600px) {
+            .share-popup button {
+                font-size: 16px;
+                padding: 15px;
+            }
+        }
+
+
         .overlay {
+            display: none;
             position: fixed;
             top: 0;
             left: 0;
@@ -122,7 +172,6 @@
         .liked {
             color: red;
         }
-
         .mute-icon {
             position: absolute;
             top: 50%;
